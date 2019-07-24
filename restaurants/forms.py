aa4c21dbfc93f0b16c65749bc60868a05ffbd3f5
mshalhadhrawi <mshalhad111@gmail.com>
2019-07-24 17:16:45 +0300

    done task_07

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -4,5 +4,4 @@ from .models import Restaurant
 class RestaurantForm(forms.ModelForm):
     class Meta:
         model = Restaurant
- #fields=['name','description','opening_time','closing_time']
         fields = '__all__'
